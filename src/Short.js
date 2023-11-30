4712e20e26259e03f3aaea10284779f45b93775e
Aung-Kaung-Mon <aungkaungmon.2020.20@gmail.com>
2023-11-30 18:31:49 +0630

    late

--- src/Short.js
+++ src/Short.js
@@ -1,14 +1,36 @@
 import { observer } from "./app/cartui";
 import { categoryRender } from "./app/category";
 import { productRender } from "./app/products";
-import { cartBtnHandler, orderBtnHandler, searchBtnFocusHandler } from "./core/handlers";
+import {
+  cartBtnHandler,
+  orderBtnHandler,
+  searchBtnFocusHandler,
+} from "./core/handlers";
 import { cartBtn, closeBtn, orderBtn } from "./core/selectors";
 import { categories, products } from "./core/variables";
 
 export default class Short {
+  // async preRender() {
+  //   // categoryRender(categories);
+  //   // productRender(products);
+
+  //   const res2 = await fetch("http://localhost:5173/api/categories");
+  //   const json2 = await res2.json();
+  //   categoryRender(json2);
+
+  //   const res = await fetch("http://localhost:5173/api/products");
+  //   const json = await res.json();
+  //   productRender(json);
+  // }
+
   preRender() {
-    categoryRender(categories);
-    productRender(products);
+    fetch("http://localhost:5173/api/categories")
+      .then((res) => res.json())
+      .then((json) => categoryRender(json));
+
+    fetch("http://localhost:5173/api/products")
+      .then((res) => res.json())
+      .then((json) => productRender(json));
   }
 
   listener() {
